96e7bc5ebb05ce72f73b05bf9eb087f794fa7325
Morgan <up85972@umbc.edu>
2015-12-08 17:18:35 -0500

    Update AdminEditInd.php

--- Project2/AdminEditInd.php
+++ Project2/AdminEditInd.php
@@ -124,6 +124,9 @@ session_start();
 		  
 	</div>
 	</div>
+	<footer>
+	<?php include("footer.html"); ?>
+  	</footer>
 	<div class="bottom">
 		<p style='color:red'>Please note that individual appointments can only be removed from schedule.</p>
 	</div>
@@ -131,9 +134,7 @@ session_start();
 	<?php include('./workOrder/workButton.php'); ?>
 
 	</div>
-  <footer>
-	<?php include("footer.html"); ?>
-  </footer>
+  
   </body>
   
 </html>
